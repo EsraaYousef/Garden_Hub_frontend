6de7308819d12d1266cf21579f0541929a7c8ba7
exit <esraayousef400@gmail.com>
2021-08-24 18:55:01 +0200

    upload edits

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -2,6 +2,7 @@ $(document).ready(function () {
   "use strict";
 
   AOS.init({
+    once: true,
     // delay: 1500, // values from 0 to 3000, with step 50ms
     // duration: 900, // values from 0 to 3000, with step 50ms
     easing: "ease-out-back", // default easing for AOS animations
@@ -41,6 +42,9 @@ $(document).ready(function () {
     $(".navbar-collapse.show ul.navbar-nav.main-navbar.open").removeClass(
       "open"
     );
+  });
+  $(".add-textBox").change(function () {
+    $(this).parent().find(".form-control").prop("hidden", !this.checked);
   });
 });
 
@@ -115,7 +119,88 @@ function previewFile(input, image) {
     false
   );
 })();
+//vertical Wizard
+$(document).ready(function () {
+  $('.vertical-tabs button[data-bs-toggle="tab"]').on(
+    "show.bs.tab",
+    function (e) {
+      var $target = $(e.target);
+      $(window).scrollTop(0);
+      if ($target.parent().hasClass("disabled")) {
+        return false;
+      }
+    }
+  );
 
+  $(".vertical-tabs .next-step").click(function (e) {
+    var $active = $(".vertical-tabs .nav-tabs li> .nav-link.active");
+    $active
+      .parent()
+      .next()
+      .find(".vertical-tabs .nav-link")
+      .removeClass("disabled");
+    nextTab($active);
+  });
+
+  $(".vertical-tabs .prev-step").click(function (e) {
+    var $active = $(".vertical-tabs .nav-tabs li> .nav-link.active");
+    prevTab($active);
+  });
+});
+function nextTab(elem) {
+  $(elem).parent().next().find('button[data-bs-toggle="tab"]').click();
+}
+function prevTab(elem) {
+  $(elem).parent().prev().find('button[data-bs-toggle="tab"]').click();
+}
+//END WIZARD
+
+//new update
+$(document).ready(function () {
+  //sub tab
+  $(".sub-tab.step-two").addClass("hidden");
+
+  $(".sub-tab .control-label").click(function () {
+    setTimeout(function () {
+      $(".sub-tab.step-one").addClass("hidden ease");
+      $(".sub-tab.step-two").removeClass("hidden").addClass("visible ease");
+    }, 1000);
+  });
+
+  $(".sub-tab .back-label").click(function () {
+    $(".step-one").removeClass("hidden ease");
+    $(".sub-tab.step-two").removeClass("visible ease").addClass("hidden");
+  });
+  $(".sub-tab #close_sub_tab").click(function () {
+    $(".step-one").removeClass("hidden ease");
+    $(".sub-tab.step-two").removeClass("visible ease").addClass("hidden");
+  });
+
+  if (window.File && window.FileList && window.FileReader) {
+    $("input[type=file]").on("change", function (e) {
+      var files = e.target.files,
+        filesLength = files.length;
+      for (var i = 0; i < filesLength; i++) {
+        var f = files[i];
+        var fileReader = new FileReader();
+        var newImg = $(this).next();
+        console.log("newImg", newImg);
+        var str = $(this)[0].files[0].name;
+        console.log(str);
+
+        fileReader.onload = function (e) {
+          // $(newImg).next().css("display", "none");
+          $(newImg).next().html(str);
+          console.log("result", e.target.result);
+          newImg.attr("src", e.target.result);
+        };
+        fileReader.readAsDataURL(f);
+      }
+    });
+  } else {
+    alert("Your browser doesn't support File");
+  }
+});
 // loading window
 $(window).on("load", function () {
   $("body").css("overflow", "hidden");
